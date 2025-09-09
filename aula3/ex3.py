c8954f5b65b254cf7b3cc324faddf8a52423e503
Gabi <gabrielle.sote@gmail.com>
2025-09-09 19:39:12 -0300

    Create ex3.py

---
+++ aula3/ex3.py
@@ -0,0 +1,21 @@
+i = 10
+if(i > 15):
+  print("10 é menor do que 15")
+print("Não está no IF")
+
+i = 10
+if(i < 15):
+  print("é menor do que 15")
+else:
+  print("é maior do que 15")
+print("Esta fora do bloco else")
+
+i = 20
+if(i == 10):
+  print("i is 10")
+elif(i == 15):
+  print("i is 15")
+elif(i == 20):
+  print("i is 20")
+else:
+  print("nenhuma das condições anteriores")
