39bb1fda4454ca6fe1624233aad6c9d97ef4e83b
Gabi <gabrielle.sote@gmail.com>
2025-09-16 19:51:32 -0300

    Create aula5.py

---
+++ aula5.py
@@ -0,0 +1,18 @@
+# Sobre dict()
+
+Dict = {}
+
+print(Dict)
+
+Dict[0] = "Gabi"
+Dict[1] = "Henrique"
+Dict[2] = "Paloma"
+
+print(Dict)
+
+Dict["Valor"] = 1,2,3,4
+
+print(Dict)
+
+Dict[2] = "Atualização"
+print(Dict)
